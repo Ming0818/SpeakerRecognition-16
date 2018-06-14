23a2116d3e2ef82be6269382f319e1d91dd37fcf
hwijunkwon <fjrzlgnwns@naver.com>
2018-06-14 18:13:43 +0900

    downloadFromYoutube.py -유투브를 통한 동영상 파일 저장 및 wav파일로 변환

---
+++ 데이터 전처리/Mel Spectrogram/librosa-feature-melspectrogram-1.py
@@ -0,0 +1,37 @@
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+p_dir = "C:\\Users\\kwon\\Desktop\\개인 프로젝트\\화자인식을 위한 CNN 기반 음성인식 알고리즘의 개발\\데이터\\"
+num_input = 1
+num_file = 1
+
+def processing(parentDir, filename, num):
+    y, sr = librosa.load(parentDir + filename, sr=None)
+    librosa.feature.melspectrogram(y=y, sr=sr)
+
+    #고속 퓨리에 변환 결과의 절대값에 2승을 한다
+    D = np.abs(librosa.stft(y))**2
+    S = librosa.feature.melspectrogram(S=D)
+
+    # Passing through arguments to the Mel filters
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=96, fmax=18000)
+
+    print(S.shape)
+
+    fig = plt.figure(figsize=(8.62, 0.96))
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', fmax=18000, x_axis='time')
+    plt.subplots_adjust(left=0, right=1, bottom=0, top = 1, wspace=0, hspace=0)
+    plt.set_cmap(cmap='Greys')
+    plt.axis('off')
+    plt.savefig(parentDir+"melspec"+ num +".png")
+    plt.show()
+
+
+for i in range(num_input):
+    for j in range(num_file):
+        processing(p_dir + str(i+1) + "\\", "input" + str(j+1) + ".wav", str(j+1))
+
+
+
+
